1067961fad307afd8d227672b4afe52d7ca15b86
Mike McEwen <mcewball13@gmail.com>
2023-09-23 13:33:30 -0400

    added in remove whitespace functionality

--- newline_checker.py
+++ newline_checker.py
@@ -6,6 +6,24 @@ def get_last_byte(file_path):
         f.seek(-1, os.SEEK_END)  # Go to the last byte.
         return f.read(1)
 
+def remove_trailing_whitespace_and_check(file_path):
+    with open(file_path, 'r') as f:
+        lines = f.readlines()
+
+    # If no lines or last line is not whitespace, return False
+    if not lines or lines[-1].strip() != '':
+        return False
+
+    # Remove trailing empty lines or lines with only whitespace
+    while lines and lines[-1].strip() == '':
+        lines.pop()
+
+    with open(file_path, 'w') as f:
+        f.writelines(lines)
+    
+    return True
+
+
 def main(directory):
     NEWLINES_ADDED = 0  # Counter for newlines added
 
@@ -13,11 +31,13 @@ def main(directory):
     user_choices = {}
 
     for root, dirs, files in os.walk(directory):
-        # Skip 'node_modules' and '.git' directories
-        if 'node_modules' in dirs:
-            dirs.remove('node_modules')
-        if '.git' in dirs:
-            dirs.remove('.git')
+        # Skip directories starting with '.' including '.git' and 'node_modules'
+        if any(dir.startswith('.') for dir in dirs):
+            dirs[:] = [dir for dir in dirs if not dir.startswith('.')]
+        
+        # This will ensure we don't process hidden directories or their contents
+        if '/.' in root:
+            continue
 
         for file in files:
             # Skip hidden files
@@ -36,6 +56,8 @@ def main(directory):
                 # Always ask for files with no extension
                 answer = input(f"\033[33mFile has no extension. Do you want to process this file? (y/n) \033[0m")
                 process_file = answer.lower() == "y"
+            elif ext in extensions:
+                process_file = True
             elif ext not in extensions:
                 if ext not in user_choices:
                     answer = input(f"\033[33mUnrecognized extension .{ext}. Do you want to process this kind of file? (y/n) \033[0m")
@@ -45,6 +67,7 @@ def main(directory):
 
                 process_file = answer.lower() == "y"
 
+
             if not process_file:
                 print(f"Skipping file: {file_path}")
                 continue
@@ -52,7 +75,13 @@ def main(directory):
             last_byte = get_last_byte(file_path)
             print(f"Last byte of {file_path} is: {last_byte.hex()}")
 
-            if last_byte.hex() != "0a":
+           # If trailing whitespace was removed, inform the user and continue
+            if remove_trailing_whitespace_and_check(file_path):
+                print(f"\033[31mWhitespace removed from the end of {file_path}.\033[0m")  # Red text for removed whitespace
+                continue
+
+            # If no newline at the end, add one
+            if get_last_byte(file_path).hex() != "0a":
                 print(f"\033[32mAdding newline to {file_path}.\033[0m")  # Green text for added newline
                 try:
                     with open(file_path, 'a') as f:
@@ -62,6 +91,9 @@ def main(directory):
                     print(f"\033[31mPermission denied for {file_path}. Skipping...\033[0m")
             else:
                 print(f"Skipping {file_path} - already has a newline at the end.")
+
+
+
 
     print("Done processing files!")
     print(f"{NEWLINES_ADDED} newlines were added.")
